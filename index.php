781acd7b5d44a3fe3edbd5e4c0601b06f4f44489
SamaGhaleShah <peuprv@hi2.in>
2020-12-06 16:34:46 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,107 +1,30 @@
-#!/usr/bin/env php
 <?php
-/**
- * Example bot.
- *
- * Copyright 2016-2020 Daniil Gentili
- * (https://daniil.it)
- * This file is part of MadelineProto.
- * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU General Public License along with MadelineProto.
- * If not, see <http://www.gnu.org/licenses/>.
- *
- * @author    Daniil Gentili <daniil@daniil.it>
- * @copyright 2016-2020 Daniil Gentili <daniil@daniil.it>
- * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
- *
- * @link https://docs.madelineproto.xyz MadelineProto documentation
- */
 
-use danog\MadelineProto\API;
-use danog\MadelineProto\EventHandler;
-use danog\MadelineProto\Logger;
-use danog\MadelineProto\Settings;
-use danog\MadelineProto\Settings\Database\Mysql;
-use danog\MadelineProto\Settings\Database\Postgres;
-use danog\MadelineProto\Settings\Database\Redis;
-
-/*
- * Various ways to load MadelineProto
- */
-if (\file_exists('vendor/autoload.php')) {
-    include 'vendor/autoload.php';
-} else {
-    if (!\file_exists('madeline.php')) {
-        \copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
-    }
-    /**
-     * @psalm-suppress MissingFile
-     */
-    include 'madeline.php';
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
+include 'madeline.php';
 
-/**
- * Event handler class.
- */
-class MyEventHandler extends EventHandler
-{
-    /**
-     * @var int|string Username or ID of bot admin
-     */
-    const ADMIN = "danogentili"; // Change this
-    /**
-     * Get peer(s) where to report errors.
-     *
-     * @return int|string|array
-     */
-    public function getReportPeers()
-    {
-        return [self::ADMIN];
-    }
-    /**
-     * Handle updates from supergroups and channels.
-     *
-     * @param array $update Update
-     *
-     * @return void
-     */
-    public function onUpdateNewChannelMessage(array $update): \Generator
-    {
-        return $this->onUpdateNewMessage($update);
-    }
-    /**
-     * Handle updates from users.
-     *
-     * @param array $update Update
-     *
-     * @return \Generator
-     */
-    public function onUpdateNewMessage(array $update): \Generator
-    {
-        if ($update['message']['_'] === 'messageEmpty' || $update['message']['out'] ?? false) {
-            return;
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->async(true);
+$MadelineProto->loop(function () use ($MadelineProto) {
+    yield $MadelineProto->start();
+
+    $me = yield $MadelineProto->getSelf();
+
+    $MadelineProto->logger($me);
+
+    if (!$me['bot']) {
+        yield $MadelineProto->messages->sendMessage(['peer' => '@danogentili', 'message' => "Hi!\nThanks for creating MadelineProto! <3"]);
+        yield $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+
+        try {
+            yield $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
+        } catch (\danog\MadelineProto\RPCErrorException $e) {
+            $MadelineProto->logger($e);
         }
 
-        $res = \json_encode($update, JSON_PRETTY_PRINT);
-        yield $this->messages->sendMessage(['peer' => $update, 'message' => "<code>$res</code>", 'reply_to_msg_id' => isset($update['message']['id']) ? $update['message']['id'] : null, 'parse_mode' => 'HTML']);
-        if (isset($update['message']['media']) && $update['message']['media']['_'] !== 'messageMediaGame') {
-            yield $this->messages->sendMedia(['peer' => $update, 'message' => $update['message']['message'], 'media' => $update]);
-        }
+        yield $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
     }
-}
-
-$settings = new Settings;
-$settings->getLogger()->setLevel(Logger::LEVEL_ULTRA_VERBOSE);
-
-// You can also use Redis, MySQL or PostgreSQL
-// $settings->setDb((new Redis)->setDatabase(0)->setPassword('pony'));
-// $settings->setDb((new Postgres)->setDatabase('MadelineProto')->setUsername('daniil')->setPassword('pony'));
-// $settings->setDb((new Mysql)->setDatabase('MadelineProto')->setUsername('daniil')->setPassword('pony'));
-
-$MadelineProto = new API('bot.madeline', $settings);
-
-// Reduce boilerplate with new wrapper method.
-// Also initializes error reporting, catching and reporting all errors surfacing from the event loop.
-$MadelineProto->startAndLoop(MyEventHandler::class);
+    yield $MadelineProto->echo('OK, done!');
+});
