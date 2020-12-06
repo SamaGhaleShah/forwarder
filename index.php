83813e17a1a98e02716cba96761aa69751d37539
SamaGhaleShah <peuprv@hi2.in>
2020-12-06 16:28:34 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,88 +1,30 @@
 <?php
 
-use danog\MadelineProto\API;
-use danog\MadelineProto\EventHandler;
-use danog\MadelineProto\Logger;
-use danog\MadelineProto\Settings;
-use danog\MadelineProto\Settings\Database\Mysql;
-use danog\MadelineProto\Settings\Database\Postgres;
-use danog\MadelineProto\Settings\Database\Redis;
-
-/*
- * Various ways to load MadelineProto
- */
-if (\file_exists('vendor/autoload.php')) {
-    include 'vendor/autoload.php';
-} else {
-    if (!\file_exists('madeline.php')) {
-        \copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
-    }
-    /**
-     * @psalm-suppress MissingFile
-     */
-    include 'madeline.php';
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
+include 'madeline.php';
 
-/**
- * Event handler class.
- */
-class MyEventHandler extends EventHandler
-{
-    /**
-     * @var int|string Username or ID of bot admin
-     */
-    const ADMIN = "heiio"; // Change this
-    /**
-     * Get peer(s) where to report errors.
-     *
-     * @return int|string|array
-     */
-    public function getReportPeers()
-    {
-        return [self::ADMIN];
-    }
-    /**
-     * Handle updates from supergroups and channels.
-     *
-     * @param array $update Update
-     *
-     * @return void
-     */
-    public function onUpdateNewChannelMessage(array $update): \Generator
-    {
-        return $this->onUpdateNewMessage($update);
-    }
-    /**
-     * Handle updates from users.
-     *
-     * @param array $update Update
-     *
-     * @return \Generator
-     */
-    public function onUpdateNewMessage(array $update): \Generator
-    {
-        if ($update['message']['_'] === 'messageEmpty' || $update['message']['out'] ?? false) {
-            return;
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->async(true);
+$MadelineProto->loop(function () use ($MadelineProto) {
+    yield $MadelineProto->start();
+
+    $me = yield $MadelineProto->getSelf();
+
+    $MadelineProto->logger($me);
+
+    if (!$me['bot']) {
+        yield $MadelineProto->messages->sendMessage(['peer' => '@heiio', 'message' => "Hi!\nThanks for creating MadelineProto! <3"]);
+        //yield $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+
+        try {
+            //yield $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
+        } catch (\danog\MadelineProto\RPCErrorException $e) {
+            $MadelineProto->logger($e);
         }
 
-        $res = \json_encode($update, JSON_PRETTY_PRINT);
-        yield $this->messages->sendMessage(['peer' => $update, 'message' => "<code>$res</code>", 'reply_to_msg_id' => isset($update['message']['id']) ? $update['message']['id'] : null, 'parse_mode' => 'HTML']);
-        if (isset($update['message']['media']) && $update['message']['media']['_'] !== 'messageMediaGame') {
-            yield $this->messages->sendMedia(['peer' => $update, 'message' => $update['message']['message'], 'media' => $update]);
-        }
+        yield $MadelineProto->messages->sendMessage(['peer' => '@heiio', 'message' => 'Testing MadelineProto!']);
     }
-}
-
-$settings = new Settings;
-$settings->getLogger()->setLevel(Logger::LEVEL_ULTRA_VERBOSE);
-
-// You can also use Redis, MySQL or PostgreSQL
-// $settings->setDb((new Redis)->setDatabase(0)->setPassword('pony'));
-// $settings->setDb((new Postgres)->setDatabase('MadelineProto')->setUsername('daniil')->setPassword('pony'));
-// $settings->setDb((new Mysql)->setDatabase('MadelineProto')->setUsername('daniil')->setPassword('pony'));
-
-$MadelineProto = new API('bot.madeline', $settings);
-
-// Reduce boilerplate with new wrapper method.
-// Also initializes error reporting, catching and reporting all errors surfacing from the event loop.
-$MadelineProto->startAndLoop(MyEventHandler::class);
+    yield $MadelineProto->echo('OK, done!');
+});
